af35d44c7294eb89d14d72e5b0af422a013cbd98
DIALLO Mamadou Alimou <diallomohamedalim@outlook.fr>
2026-02-09 21:01:07 +0000

    modification du site

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const message = document.getElementById("message");
 bouton.addEventListener("click", function () {
     message.textContent = "Merci de visiter ma page";
 });
+
+
+function voirPlus(id, bouton) {
+    const contenu = document.getElementById(id);
+
+    if (contenu.style.display === "none") {
+        contenu.style.display = "inline";  // Affiche le texte
+        bouton.textContent = "Voir moins"; // Change le texte du bouton
+    } else {
+        contenu.style.display = "none";    // Cache le texte
+        bouton.textContent = "Voir plus";  // Remet le texte du bouton
+    }
+}
