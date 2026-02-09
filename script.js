039672e24f561c1e4c72fa544bbd35963eb77419
Alim-Diallo <Diallomohamedalim@outlook.fr>
2026-02-09 21:22:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const bouton = document.getElementById("btnMessage");
-const message = document.getElementById("message");
-
-bouton.addEventListener("click", function () {
-    message.textContent = "Merci de visiter ma page";
-});
 
 
 function voirPlus(id, bouton) {
